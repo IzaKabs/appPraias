d6ed2a564c7b84070c6987c814cf31cb33df86da
izabella <168788884+IzaKabs@users.noreply.github.com>
2025-11-26 18:33:54 +0000

    final

--- guiaPraias/app/(tabs)/confg.tsx
+++ guiaPraias/app/(tabs)/confg.tsx
@@ -82,7 +82,7 @@ export default function ConfigScreen() {
                 <View style={styles.modalOverlay}>
                     {/* Caixa de conteúdo do modal */}
                     <View style={styles.modalContent}>
-                        <Text style={styles.modalText}>Desenvolvido por: Aluno</Text>
+                        <Text style={styles.modalText}>Desenvolvido por: Izabella Nunes Dos Santos</Text>
                         <TouchableOpacity
 
                             style={styles.modalCloseButton}
